cc901307313f29f8b086c3e8caf5109d87116117
Mahi <mahipalsinghapsit0@gmail.com>
2026-04-07 17:10:52 +0530

    added vps ip

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -99,7 +99,7 @@ if (!isAuthenticated) {
 
             <div className="flex items-center gap-2 md:gap-3 cursor-pointer" onClick={() => navigate('/')}>
               <img
-                src="/images/logo.png"
+                src="logo.png"
                 alt="CivicLens Logo"
                 className="h-6 md:h-8 w-auto object-contain"
               />
